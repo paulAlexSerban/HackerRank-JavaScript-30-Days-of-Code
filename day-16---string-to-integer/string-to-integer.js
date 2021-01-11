71b211e0748db723c76783b10190371ce69f2244
Paul Serban <paul.serban@netcentric.biz>
2021-01-11 23:50:08 +0200

    day-16---string-to-integer

---
+++ day-16---string-to-integer/string-to-integer.js
@@ -0,0 +1,10 @@
+function main() {
+  const S = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.';
+  try {
+    let ans = parseInt(S) || ans.toString();
+    console.log(parseInt(S));
+  } catch (e) {
+    console.log("Bad String");
+  }
+
+}
